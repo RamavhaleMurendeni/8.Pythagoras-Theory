618f8b37947f862b3ee03100cb03f15ea54535b6
Ramavhale Murendeni <Ramavhale Murendeni@DESKTOP-CO1SVRB>
2024-01-18 22:29:10 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.White
+        Button1.Location = New Point(157, 58)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(97, 37)
+        Button1.TabIndex = 0
+        Button1.Text = "Enter a"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(157, 121)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(97, 38)
+        Button2.TabIndex = 1
+        Button2.Text = "Enter b"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = Color.IndianRed
+        Button3.Location = New Point(136, 242)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(160, 36)
+        Button3.TabIndex = 2
+        Button3.Text = "C(Hypotemus) is Equal to"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(431, 66)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(211, 23)
+        TextBox1.TabIndex = 3
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(431, 137)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(211, 23)
+        TextBox2.TabIndex = 4
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(421, 255)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(221, 23)
+        TextBox3.TabIndex = 5
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ActiveCaption
+        ClientSize = New Size(800, 450)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Pythagoras Theory"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+End Class
